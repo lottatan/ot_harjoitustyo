bf65f4fd511285de5c5c24bb0f5aa12df60bbde1
Lotta Tanner <lotta.tanner@helsinki.fi>
2022-03-23 15:28:26 +0200

    tehtiin muutoksia jotta coverage report parantuisi

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -5,7 +5,6 @@ from maksukortti import Maksukortti
 class TestKassapaate(unittest.TestCase):
     def setUp(self):
         self.kassapaate = Kassapaate()
-        self.maksukortti = Maksukortti(1000)
 
     def test_kassapaate_on_olemassa(self):
         self.assertNotEqual(self.kassapaate, None)
@@ -41,38 +40,51 @@ class TestKassapaate(unittest.TestCase):
 
 
     def test_korttiosto_edullisesti_toimii(self):
-        tulos = self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
+        maksukortti = Maksukortti(1000)
+        tulos = self.kassapaate.syo_edullisesti_kortilla(maksukortti)
 
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
         self.assertEqual(self.kassapaate.edulliset, 1)
-        self.assertEqual(self.maksukortti.saldo, 760)
+        self.assertEqual(maksukortti.saldo, 760)
         self.assertEqual(tulos, True)
 
     def test_korttiosto_edullisesti_ei_toimi(self):
-        tulos = self.kassapaate.syo_edullisesti_kortilla(self.maksukortti)
+        maksukortti = Maksukortti(200)
+        tulos = self.kassapaate.syo_edullisesti_kortilla(maksukortti)
 
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
-        self.assertEqual(self.kassapaate.edulliset, 1)
-        self.assertEqual(self.maksukortti.saldo, 760)
-        self.assertEqual(tulos, True)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+        self.assertEqual(maksukortti.saldo, 200)
+        self.assertEqual(tulos, False)
 
     def test_korttiosto_maukkaasti_toimii(self):
-        tulos = self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti)
+        maksukortti = Maksukortti(1000)
+        tulos = self.kassapaate.syo_maukkaasti_kortilla(maksukortti)
 
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
         self.assertEqual(self.kassapaate.maukkaat, 1)
-        self.assertEqual(self.maksukortti.saldo, 600)
+        self.assertEqual(maksukortti.saldo, 600)
         self.assertEqual(tulos, True)
 
     def test_korttiosto_maukkaasti_ei_toimi(self):
-        tulos = self.kassapaate.syo_maukkaasti_kortilla(self.maksukortti)
+        maksukortti = Maksukortti(300)
+        tulos = self.kassapaate.syo_maukkaasti_kortilla(maksukortti)
 
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
-        self.assertEqual(self.kassapaate.maukkaat, 1)
-        self.assertEqual(self.maksukortti.saldo, 600)
-        self.assertEqual(tulos, True)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+        self.assertEqual(maksukortti.saldo, 300)
+        self.assertEqual(tulos, False)
 
     def test_kortin_lataus_toimii(self):
-        self.kassapaate.lataa_rahaa_kortille(self.maksukortti, 100)
+        maksukortti = Maksukortti(1000)
+        self.kassapaate.lataa_rahaa_kortille(maksukortti, 100)
+
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100100)
-        self.assertEqual(self.maksukortti.saldo, 1100)
+        self.assertEqual(maksukortti.saldo, 1100)
+
+    def test_kortin_lataus_ei_toimi(self):
+        maksukortti = Maksukortti(1000)
+        self.kassapaate.lataa_rahaa_kortille(maksukortti, -10)
+
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+        self.assertEqual(maksukortti.saldo, 1000)
