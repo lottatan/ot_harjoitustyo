cbe0f09357a70d892cf191f0d441a7b07811404a
Lotta Tanner <lotta.tanner@helsinki.fi>
2022-03-31 15:17:46 +0300

    päiviteltiin budjettisovellusta

---
+++ budjettisovellus/src/repositories/purchase_repository.py
@@ -0,0 +1,17 @@
+#Tänne tallennetaan kaikki tehdyt ostokset
+
+class PurchaseRepository:
+    def __init__(self):
+        pass
+
+    def show_all_by_username(self):
+        pass
+
+    def add_purchase(self):
+        pass
+
+    def delete_purchase(self):
+        pass
+
+    def delete_all_purchases(self):
+        pass
